f80e19943c939d6f946c553c60ec37c4b1d326d6
EliasPhysics <elias.88348@gmail.com>
2024-09-07 01:47:21 +0200

    added data analysis module

--- junk_code/x2_approx_explanation2.py
+++ junk_code/x2_approx_explanation2.py
@@ -1,22 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def piecewise_linear(x, n):
+def piecewise_linear_approx(x, n):
+    """
+    Create a piecewise linear approximation of x^2 that is always >= x^2
+    and continuous, with n segments.
+    """
     breakpoints = np.linspace(0, 1, n+1)
     y = np.zeros_like(x)
     for i in range(n):
         mask = (x >= breakpoints[i]) & (x <= breakpoints[i+1])
-        x_i = (x[mask] - breakpoints[i]) / (breakpoints[i+1] - breakpoints[i])
-        y[mask] = breakpoints[i]**2 + 2*breakpoints[i]*(breakpoints[i+1] - breakpoints[i])*x_i
+        x0, x1 = breakpoints[i], breakpoints[i+1]
+        y0, y1 = x0**2, x1**2
+        slope = (y1 - y0) / (x1 - x0)
+        y[mask] = y0 + slope * (x[mask] - x0)
     return y
 
 x = np.linspace(0, 1, 1000)
 
 plt.figure(figsize=(8, 6))
 plt.plot(x, x**2, 'b-', linewidth=2, label='f0')
-plt.plot(x, piecewise_linear(x, 1), 'r-', linewidth=2, label='f1')
-plt.plot(x, piecewise_linear(x, 2), 'g-', linewidth=2, label='f2')
-plt.plot(x, piecewise_linear(x, 4), 'k--', linewidth=2, label='f')
+plt.plot(x, piecewise_linear_approx(x, 1), 'r-', linewidth=2, label='f1')
+plt.plot(x, piecewise_linear_approx(x, 2), 'g-', linewidth=2, label='f2')
+plt.plot(x, piecewise_linear_approx(x, 4), 'k--', linewidth=2, label='f')
 
 plt.xlim(0, 1)
 plt.ylim(0, 1)
